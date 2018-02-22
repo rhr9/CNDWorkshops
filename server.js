35ebbec0a377b9c20de70b9423e4f943b9e3a4ff
rhr9 <rene.vanderhorst@yahoo.com>
2018-02-22 23:49:36 +0000

    changed welcome message

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ function convertToCaps(str) {
 }
 
 app.get('/', function(req,res) {
-    res.end('Milk was a bad choice');
+    res.end('Milk was a bad choice for everyone');
 });
 
 app.post('/api/capitalise', function(req, res) {
